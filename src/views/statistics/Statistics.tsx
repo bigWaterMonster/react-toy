3dafb17903a83acc47acca0595fa1b60e19b859b
bigWaterMonster <407982063@qq.com>
2020-05-16 21:09:07 +0800

    展示记录

--- src/views/statistics/Statistics.tsx
+++ src/views/statistics/Statistics.tsx
@@ -4,15 +4,28 @@ import { CategorysSectionWrapper } from 'views/money/components/CategorysSection
 import styled from 'styled-components'
 import { useRecords } from 'views/hooks/useRecords'
 import { useTags } from 'views/hooks/useTags'
+import day from 'dayjs'
 
 const Wrapper = styled.div`background: white;`
 
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+  > .note {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+`
 
 function Statistics() {
   const [category, setCategory] = useState(0)
   const { records } = useRecords()
   const { getName } = useTags()
-  console.log(getName(4))
   return (
     <Layout>
       <Wrapper>
@@ -21,14 +34,18 @@ function Statistics() {
       <div>
         {
           records.map((record, index) => {
-            return <div key={index}>
+            return <Item key={index}>
               {
                 record.tags.map(id => {
                   return <div key={id}>{getName(id)!.name}</div>
                 })
               }
-              {/* {record.amount} */}
-            </div>
+
+              {record.note && <div className="note">{record.note}</div>}
+              {/* {day(record.createTime as string).format('YYYY年MM月DD日')}
+              <hr /> */}
+              <div>¥{record.amount}</div>
+            </Item  >
           })
         }
       </div>
